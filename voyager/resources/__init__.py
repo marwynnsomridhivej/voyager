0213906fce7c1b2aec12f97797621d4bbff852f7
marwynnsomridhivej <51054391+marwynnsomridhivej@users.noreply.github.com>
2020-11-07 14:26:02 -0500

    added models, note yet to implement

--- voyager/resources/__init__.py
+++ voyager/resources/__init__.py
@@ -1,7 +1,9 @@
 from .apodresource import *
 from .base import *
+from .cadresource import *
 from .cmeresource import *
 from .earthresource import *
+from .epicresource import *
 from .fireballresource import *
 from .flrresource import *
 from .gstresource import *
@@ -16,3 +18,16 @@ from .sepresource import *
 from .techportresource import *
 from .techtransferresource import *
 from .wsaresource import *
+
+# TODO: from .eonetresource import * (eonet)
+# TODO: from .exoplanetresource import * (exoplanet)
+# TODO: from .genelabresource import * (genelab)
+# TODO: from .insightresource import * (insight - mars weather service)
+# TODO from .nasamediaresource import * (nasa image and video library)
+# TODO: from .sscresource import * (satellite situation center)
+# TODO: from .mdresource import * (SSD/CNEOS mission design)
+# TODO: from .nhatsresource import * (SSD/CNEOS nhats)
+# TODO: from .scoutresource import * (SSD/CNEOS scout)
+# TODO: from .sentryresource import * (SSD/CNEOS sentry)
+# TODO: from .tleresource import * (TLE)
+# TODO: from .wmtsresource mport * (Vesta/Moon/Mars Trek WMTS)
